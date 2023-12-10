5566a461e4c9407501c3695880b0d159d5a52aa7
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2023-12-10 20:17:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const audio = document.getElementById("myAudio");
+  const toggleBtn = document.getElementById("toggleBtn");
+  const stopBtn = document.getElementById("stopBtn");
+  const volumeRange = document.getElementById("volumeRange");
+
+  function toggleAudio() {
+    if (audio.paused || audio.ended) {
+      audio.play();
+      toggleBtn.textContent = "Pause";
+    } else {
+      audio.pause();
+      toggleBtn.textContent = "Play";
+    }
+  }
+
+  function stopAudio() {
+    audio.pause();
+    audio.currentTime = 0;
+    toggleBtn.textContent = "Play";
+  }
+
+  toggleBtn.addEventListener("click", toggleAudio);
+  stopBtn.addEventListener("click", stopAudio);
+
+  volumeRange.addEventListener("input", function () {
+    audio.volume = volumeRange.value;
+  });
+});
